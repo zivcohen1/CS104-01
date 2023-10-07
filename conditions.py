a1b3b8250cef7ce4f86db67769343d801a5b4c70
zivcohen1 <146225919+zivcohen1@users.noreply.github.com>
2023-10-07 17:38:57 -0400

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,25 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:21:31) [MSC v.1936 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: C:\Users\zivco\OneDrive\Documents\conditions.py ===========
+Please enter the current temperature (type '999' to end the program): 78
+Short sleeves are fine
+Please enter the current temperature (type '999' to end the program): 90
+Short sleeves are fine
+Please enter the current temperature (type '999' to end the program): 99
+Wear Shorts
+Please enter the current temperature (type '999' to end the program): 54
+Wear a jacket
+Please enter the current temperature (type '999' to end the program): 3
+stay inside
+Please enter the current temperature (type '999' to end the program): 45
+Wear a heavy coat
+Please enter the current temperature (type '999' to end the program): 68
+Wear a jacket
+Please enter the current temperature (type '999' to end the program): 0
+stay inside
+Please enter the current temperature (type '999' to end the program): -098
+stay inside
+Please enter the current temperature (type '999' to end the program): 999
+Wear Shorts
+End of program thank you
