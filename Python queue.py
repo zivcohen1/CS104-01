9b36595aa28a93ac6294730fbaffc479a4049dcc
zivcohen1 <146225919+zivcohen1@users.noreply.github.com>
2023-11-28 09:08:23 -0500

    Add files via upload

---
+++ Python queue.py
@@ -0,0 +1,19 @@
+names=[]
+#names= ('Joe, Sally, Jim, June, Betty, Bill, Sara, Zak, Anne, Kate')
+for x in range (10):
+    name=input("Enter name: ")
+    names.append(name)
+print(names)
+for x in range(len(names)):
+    print(names.pop(0))
+print(names)
+
+
+
+
+
+
+                    
+    
+    
+
